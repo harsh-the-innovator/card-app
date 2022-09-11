fabd69813da69835fd562756bfaa6c3529b3f0fd
harsh gandhi <harshgandhi043@gmail.com>
2022-09-11 21:58:40 +0530

    Create a function for overlay text on image

--- app/src/main/java/com/example/cardapp/workers/CardWorker.java
+++ app/src/main/java/com/example/cardapp/workers/CardWorker.java
@@ -1,12 +1,24 @@
 package com.example.cardapp.workers;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.cardapp.constants.Constants;
+import com.example.cardapp.utils.CardWorkerUtils;
+
+import java.util.Objects;
+
 public class CardWorker extends Worker {
+    private static final String TAG = "CardWorker";
     public CardWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
@@ -14,6 +26,37 @@ public class CardWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        Context applicationContext = getApplicationContext();
+        CardWorkerUtils.makeStatusNotification("Writing quote onto image",applicationContext);
+
+        CardWorkerUtils.sleep();
+
+        String imageResourceUri = getInputData().getString(Constants.KEY_IMAGE_URI);
+        String quote = getInputData().getString(Constants.CUSTOM_QUOTE);
+
+        ContentResolver contentResolver = applicationContext.getContentResolver();
+
+        //Create the bitmap
+        try {
+            Bitmap photo = BitmapFactory.decodeStream(contentResolver.openInputStream(Uri.parse(imageResourceUri)));
+
+            // Write text quote on image
+            assert quote != null;
+            Bitmap output = CardWorkerUtils.overlayTextOnBitmap(photo,applicationContext,quote);
+
+            // write bitmap to temp file
+            Uri outputUri = CardWorkerUtils.writeBitmapToFile(applicationContext,output);
+
+            Data outputData = new Data.Builder()
+                    .putString(Constants.KEY_IMAGE_URI, Objects.requireNonNull(outputUri).toString())
+                    .build();
+
+
+            return Result.success(outputData);
+        } catch (Throwable e) {
+            Log.i(TAG, "doWork: Error writing quote on image");
+            return Result.failure();
+        }
+
     }
 }
--- app/src/main/java/com/example/cardapp/workers/SaveCardToFileWorker.java
+++ app/src/main/java/com/example/cardapp/workers/SaveCardToFileWorker.java
@@ -1,12 +1,34 @@
 package com.example.cardapp.workers;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.cardapp.R;
+import com.example.cardapp.constants.Constants;
+import com.example.cardapp.utils.CardWorkerUtils;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class SaveCardToFileWorker extends Worker {
+    private static final String TAG = "SaveCardToFileWorker";
+    public static final String TITLE = "Card Image";
+    public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm:ss z", Locale.ENGLISH);
+
     public SaveCardToFileWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
@@ -14,6 +36,69 @@ public class SaveCardToFileWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Context applicationContext = getApplicationContext();
+        CardWorkerUtils.makeStatusNotification("Saving Image",applicationContext);
+        CardWorkerUtils.sleep();
+
+        ContentResolver contentResolver = applicationContext.getContentResolver();
+        
+        try {
+            String resourceUri = getInputData().getString(Constants.KEY_IMAGE_URI);
+            Bitmap bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(Uri.parse(resourceUri)));
+
+            Uri outputUri = saveImage(bitmap,contentResolver,applicationContext);
+            if(outputUri==null || TextUtils.isEmpty(outputUri.toString())){
+                return Result.failure();
+            }
+
+            Data outputData = new Data.Builder()
+                    .putString(Constants.KEY_IMAGE_URI,outputUri.toString())
+                    .build();
+
+
+            return Result.success(outputData);
+        }catch (Exception e) {
+            Log.i(TAG, "doWork: Unable to save image to Gallery", e);
+            return Result.failure();
+        }
+    }
+
+    private Uri saveImage(Bitmap bitmap,ContentResolver contentResolver,Context context) {
+        ContentValues values = getContentValues();
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + context.getString(R.string.app_name));
+        values.put(MediaStore.Images.Media.IS_PENDING, true);
+
+        Uri uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        if (uri != null) {
+            try {
+                saveImageToStream(bitmap, contentResolver.openOutputStream(uri));
+                values.put(MediaStore.Images.Media.IS_PENDING, false);
+                contentResolver.update(uri, values, null, null);
+                return uri;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
         return null;
+    }
+
+    private ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+        values.put(MediaStore.Images.Media.DATE_ADDED,DATE_FORMATTER.format(new Date()));
+        values.put(MediaStore.Images.Media.DATE_TAKEN,DATE_FORMATTER.format(new Date()));
+        return values;
+    }
+
+    private void saveImageToStream(Bitmap bitmap, OutputStream outputStream) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
